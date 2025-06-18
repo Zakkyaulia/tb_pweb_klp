87127c3e8e4106d5d04b2d6fe34d96612cb9d28b
Zakky Aulia Aldrin <zakkyaulialdrin@gmail.com>
2025-06-18 23:18:12 +0700

    admin zakky terbaru

--- src/routes/adminroute.js
+++ src/routes/adminroute.js
@@ -24,14 +24,14 @@ router.get('/requests/diproses', async (req, res) => {
   }
 });
 
-router.get('/requests/disetujui', async (req, res) => {
+router.get('/requests/selesai', async (req, res) => {
   try {
-    const requests = await request_surat.findAll({ where: { status: 'disetujui' } });
-    console.log('Requests disetujui:', requests); // Tambahkan logging
-    res.render('admin/ajuandisetujui', { requests: requests || [] });
+    const requests = await request_surat.findAll({ where: { status: 'selesai' } });
+    console.log('Requests selesai:', requests); // Tambahkan logging
+    res.render('admin/ajuanselesai', { requests: requests || [] });
   } catch (error) {
     console.error('Error fetching requests:', error);
-    res.status(500).render('admin/ajuandisetujui', { requests: [], error: 'Error fetching requests' });
+    res.status(500).render('admin/ajuanselesai', { requests: [], error: 'Error fetching requests' });
   }
 });
 
