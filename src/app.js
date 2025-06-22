5b8796f9f77f1e23a64fb9a8f84c57fcc9769a83
lolyameliaa <lolyamelia@gmail.com>
2025-06-22 23:36:10 +0700

    awi

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const session = require('express-session');
 const app = express();
-const PORT = 3002;
+const PORT = 3000;
 
 app.use(session({
   secret: 'your-secret-key',
